59c8800af2da04584e15dd64d80a9478af4dd983
sowju-d <dsowjanya440@gmail.com>
2026-05-30 17:41:33 +0530

    Updates

---
+++ static/js/488.8a8793c6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkinspirito_dashboard=self.webpackChunkinspirito_dashboard||[]).push([[488],{2488(){new WeakMap;new Set}}]);
+//# sourceMappingURL=488.8a8793c6.chunk.js.map
